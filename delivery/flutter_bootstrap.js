73fffeb7393b58a77149f96201b8713e79832d46
emrekiris <emrekiris@outlook.com>
2026-07-09 23:45:19 +0300

    🚀 Delivery web: profil kartı + SW kapalı + cache-busting (v1783629915)

--- delivery/flutter_bootstrap.js
+++ delivery/flutter_bootstrap.js
@@ -6,11 +6,11 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d6a","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=1783628938"}]};
+_flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d6a","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=1783629915"}]};
 
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2000156037"
+    serviceWorkerVersion: "159379520"
   }
 });
